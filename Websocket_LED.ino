db0f182e06f98c2add6404235ba218564b355d4f
AgusEtchissure <119890390+AgusEtchissure@users.noreply.github.com>
2023-08-04 15:11:47 -0300

    Update Websocket_LED.ino
    Se suma el envío de mensajes por puerto serial

--- Websocket_LED.ino
+++ Websocket_LED.ino
@@ -1,143 +1,101 @@
-// Import required libraries
+#include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <AsyncWebSocket.h>
 
-// Replace with your network credentials
-const char* ssid = "WUTEC-Estudiantes";
-const char* password = "ley19043";
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+const char* ssid = "WUTEC-Estudiantes";          // Tu SSID de WiFi
+const char* password = "ley19043";  // Tu contraseña de WiFi
 
 bool ledState = 0;
 const int ledPin = 2;
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
 <head>
-  <title>ESP Web Server</title>
+  <title>ESP Web Server - Mi Servidor LED</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:,">
   <style>
-  html {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-  }
-  h1 {
-    font-size: 1.8rem;
-    color: white;
-  }
-  h2{
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: #143642;
-  }
-  .topnav {
-    overflow: hidden;
-    background-color: #143642;
-  }
-  body {
-    margin: 0;
-  }
-  .content {
-    padding: 30px;
-    max-width: 600px;
-    margin: 0 auto;
-  }
-  .card {
-    background-color: #F8F7F9;;
-    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
-    padding-top:10px;
-    padding-bottom:20px;
-  }
-  .button {
-    padding: 15px 50px;
-    font-size: 24px;
-    text-align: center;
-    outline: none;
-    color: #fff;
-    background-color: #0f8b8d;
-    border: none;
-    border-radius: 5px;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-   }
-   /*.button:hover {background-color: #0f8b8d}*/
-   .button:active {
-     background-color: #0f8b8d;
-     box-shadow: 2 2px #CDCDCD;
-     transform: translateY(2px);
-   }
-   .state {
-     font-size: 1.5rem;
-     color:#8c8c8c;
-     font-weight: bold;
-   }
+    /* CSS personalizado */
+    html {
+      font-family: Arial, Helvetica, sans-serif;
+      text-align: center;
+    }
+    h1 {
+      font-size: 2rem;
+      color: #143642;
+      margin-bottom: 20px;
+    }
+    .button {
+      padding: 15px 50px;
+      font-size: 24px;
+      text-align: center;
+      outline: none;
+      color: #fff;
+      background-color: #0f8b8d;
+      border: none;
+      border-radius: 5px;
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      -webkit-tap-highlight-color: rgba(0,0,0,0);
+    }
+    .button:active {
+      background-color: #0f8b8d;
+      box-shadow: 2 2px #CDCDCD;
+      transform: translateY(2px);
+    }
   </style>
-<title>ESP Web Server</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="data:,">
 </head>
 <body>
-  <div class="topnav">
-    <h1>Servidor que enciende y apaga la luz de Jesucristo</h1>
-  </div>
-  <div class="content">
-    <div class="card">
-      <h2>SERVIDOR LED ESP32</h2>
-      <p class="state">state: <span id="state">%STATE%</span></p>
-      <p><button id="button" class="button">Apreta aca guacho</button></p>
-    </div>
-  </div>
-<script>
-  var gateway = `ws://${window.location.hostname}/ws`;
-  var websocket;
-  window.addEventListener('load', onLoad);
-  function initWebSocket() {
-    console.log('Trying to open a WebSocket connection...');
-    websocket = new WebSocket(gateway);
-    websocket.onopen    = onOpen;
-    websocket.onclose   = onClose;
-    websocket.onmessage = onMessage; // <-- add this line
-  }
-  function onOpen(event) {
-    console.log('Connection opened');
-  }
-  function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
-  }
-  function onMessage(event) {
-    var state;
-    if (event.data == "1"){
-      state = "ON";
+  <h1>Mi Servidor LED ESP32</h1>
+  <button class="button" onclick="sendMessage('toggle')">Click here</button>
+  <script>
+    var gateway = `ws://${window.location.hostname}/ws`;
+    var websocket;
+    window.addEventListener('load', onLoad);
+    function initWebSocket() {
+      console.log('Trying to open a WebSocket connection...');
+      websocket = new WebSocket(gateway);
+      websocket.onopen = onOpen;
+      websocket.onclose = onClose;
+      websocket.onmessage = onMessage;
     }
-    else{
-      state = "OFF";
+    function onOpen(event) {
+      console.log('Connection opened');
     }
-    document.getElementById('state').innerHTML = state;
-  }
-  function onLoad(event) {
-    initWebSocket();
-    initButton();
-  }
-  function initButton() {
-    document.getElementById('button').addEventListener('click', toggle);
-  }
-  function toggle(){
-    websocket.send('toggle');
-  }
-</script>
+    function onClose(event) {
+      console.log('Connection closed');
+      setTimeout(initWebSocket, 2000);
+    }
+    function onMessage(event) {
+      var state;
+      if (event.data == "1") {
+        state = "ON";
+      } else {
+        state = "OFF";
+      }
+      console.log('Received message: ' + event.data);
+    }
+    function onLoad(event) {
+      initWebSocket();
+    }
+    function sendMessage(message) {
+      websocket.send(message);
+    }
+  </script>
 </body>
 </html>
 )rawliteral";
+
 
 void notifyClients() {
   ws.textAll(String(ledState));
@@ -149,7 +107,14 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     data[len] = 0;
     if (strcmp((char*)data, "toggle") == 0) {
       ledState = !ledState;
+      digitalWrite(ledPin, ledState);
       notifyClients();
+
+      if (ledState) {
+        Serial.println("Hola");
+      } else {
+        Serial.println("Adios");
+      }
     }
   }
 }
@@ -172,53 +137,28 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
   }
 }
 
-void initWebSocket() {
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-}
-
-String processor(const String& var){
-  Serial.println(var);
-  if(var == "STATE"){
-    if (ledState){
-      return "ON";
-    }
-    else{
-      return "OFF";
-    }
-  }
-  return String();
-}
-
 void setup(){
-  // Serial port for debugging purposes
   Serial.begin(9600);
-
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, ledState);
   
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-
-  // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
 
-  initWebSocket();
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 
-  // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
+    request->send_P(200, "text/html", index_html);
   });
 
-  // Start server
   server.begin();
 }
 
 void loop() {
   ws.cleanupClients();
-  digitalWrite(ledPin, ledState);
 }
